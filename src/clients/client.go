43c4fdab8355937ee69196c92ff6731aa196f847
dunshan_zeng <dunshan_zeng@aiagain.com>
2021-09-01 19:51:13 +0800

    1

--- src/clients/client.go
+++ src/clients/client.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"grpc-kard/src/services/pbs"
+	"log"
 
-func main(){
+	"google.golang.org/grpc"
+)
 
-	
+func main() {
+	conn, err := grpc.Dial(":8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	sarwlerClient := pbs.NewCarwlerServiceClient(conn)
+	searchResponse, err := sarwlerClient.Search(context.Background(), &pbs.SearchRequest{Q: "client good!"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(searchResponse)
 }
